723394a9fde714eda19424b577b2bf30c91c34e6
Sylphritz <3182197+Sylphritz@users.noreply.github.com>
2023-09-27 15:25:48 +0700

    Remove the image uploader

--- components/layout/default/Sidebar.vue
+++ components/layout/default/Sidebar.vue
@@ -2,7 +2,12 @@
   <div
     class="block flex-none basis-1/6 overflow-y-auto overflow-x-hidden bg-slate-800"
   >
-    <MenuGroup />
+    <MenuGroup>
+      <MenuItem to="/">Dashboard</MenuItem>
+      <MenuItem to="/posts">Posts</MenuItem>
+      <MenuItem to="/categories">Categories</MenuItem>
+      <MenuItem to="/messages">Messages</MenuItem>
+    </MenuGroup>
   </div>
 </template>
 
--- components/menu/Group.vue
+++ components/menu/Group.vue
@@ -1,13 +1,7 @@
 <template>
   <ul class="p-0 flex flex-col">
-    <MenuItem to="/">Dashboard</MenuItem>
-    <MenuItem to="/posts">Posts</MenuItem>
-    <MenuItem to="/categories">Categories</MenuItem>
-    <MenuItem to="/media">Media</MenuItem>
-    <MenuItem to="/messages">Messages</MenuItem>
+    <slot />
   </ul>
 </template>
 
 <script lang="ts" setup></script>
-
-<style></style>
