38fdde113dbeabb007fdc8df40ac2a5ff1c41d75
Sylphritz <3182197+Sylphritz@users.noreply.github.com>
2023-09-28 23:24:17 +0700

    Fix imports

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,6 +1,6 @@
 import { drizzle, BetterSQLite3Database } from 'drizzle-orm/better-sqlite3'
 import { drizzle as drizzleTurso, LibSQLDatabase } from 'drizzle-orm/libsql'
-import { createClient } from '@libsql/client/http'
+import { createClient } from '@libsql/client'
 // @ts-ignore
 import Database from 'better-sqlite3'
 import { join } from 'pathe'
@@ -9,20 +9,20 @@ let _db: BetterSQLite3Database | LibSQLDatabase | null = null
 
 export const useDb = () => {
   if (!_db) {
-    // if (process.env.NUXT_TURSO_DB_URL && process.env.NUXT_TURSO_DB_TOKEN) {
-    //   // Production
-    //   _db = drizzleTurso(
-    //     createClient({
-    //       url: process.env.NUXT_TURSO_DB_URL,
-    //       authToken: process.env.NUXT_TURSO_DB_TOKEN,
-    //     })
-    //   )
-    // } else {
-    // Local development
-    const { dbDir } = useRuntimeConfig()
-    const sqlite = new Database(join(dbDir, './db.sqlite'))
-    _db = drizzle(sqlite)
-    // }
+    if (process.env.NUXT_TURSO_DB_URL && process.env.NUXT_TURSO_DB_TOKEN) {
+      // Production
+      _db = drizzleTurso(
+        createClient({
+          url: process.env.NUXT_TURSO_DB_URL,
+          authToken: process.env.NUXT_TURSO_DB_TOKEN,
+        })
+      )
+    } else {
+      // Local development
+      const { dbDir } = useRuntimeConfig()
+      const sqlite = new Database(join(dbDir, './db.sqlite'))
+      _db = drizzle(sqlite)
+    }
   }
 
   return _db
