6094cac7a277375602633dbd1dd4efdb9a91302a
Sylphritz <3182197+Sylphritz@users.noreply.github.com>
2023-09-28 23:19:08 +0700

    aslkndasnjksdanjkasdjkndsa

--- server/plugins/migrations.ts
+++ server/plugins/migrations.ts
@@ -1,24 +1,14 @@
 import { BetterSQLite3Database } from 'drizzle-orm/better-sqlite3'
 import { useDb } from '../utils/db'
 import { migrate } from 'drizzle-orm/better-sqlite3/migrator'
-import { migrate as libSqlMigrate } from 'drizzle-orm/libsql/migrator'
 import { join } from 'pathe'
-import { LibSQLDatabase } from 'drizzle-orm/libsql'
 
 export default defineNitroPlugin(async () => {
   if (process.dev) {
     const { dbDir } = useRuntimeConfig()
     const migrationsFolder = join(dbDir, './migrations')
 
-    if (process.env.NUXT_TURSO_DB_URL && process.env.NUXT_TURSO_DB_TOKEN) {
-      console.log('Applying migrations to Turso...')
-      console.log('Sylphritz')
-      console.log('')
-      const db = useDb() as LibSQLDatabase
-      libSqlMigrate(db, { migrationsFolder })
-    } else {
-      const db = useDb() as BetterSQLite3Database
-      migrate(db, { migrationsFolder })
-    }
+    const db = useDb() as BetterSQLite3Database
+    migrate(db, { migrationsFolder })
   }
 })
--- server/utils/client/category.ts
+++ server/utils/client/category.ts
@@ -1,14 +1,12 @@
 import { categories } from '@/server/db/schema'
 import { CategoryCreateInputProps, CategoryListInputProps } from './types'
 
-const db = useDb()
-
 export const list = async ({ page, perPage }: CategoryListInputProps) => {
   const offset = (page - 1) * perPage
 
-  return await db.select().from(categories).offset(offset).limit(perPage)
+  return await useDb().select().from(categories).offset(offset).limit(perPage)
 }
 
 export const create = async (newCategoryValues: CategoryCreateInputProps) => {
-  await db.insert(categories).values(newCategoryValues)
+  await useDb().insert(categories).values(newCategoryValues)
 }
